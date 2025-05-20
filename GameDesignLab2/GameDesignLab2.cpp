4aa44e0fbefdaf2280241e1923481a2c81916594
Charles Stoeter <charlesstoeter@gmail.com>
2025-05-20 10:59:41 -0400

    added keyboard and event queue

--- GameDesignLab2/GameDesignLab2.cpp
+++ GameDesignLab2/GameDesignLab2.cpp
@@ -24,6 +24,16 @@ int main()
 		std::cerr << "Failed to initialize primitives addon!" << std::endl;
 		return -1;
 	}
+
+	ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+
+
+	// Initialize Allegro Keyboard
+	if (!al_install_keyboard()) {
+		std::cerr << "Failed to initialize keyboard!" << std::endl;
+		return -1;
+	}
+
 	// Create a display
 	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
 	if (!display) {
@@ -52,29 +62,3 @@ int main()
 	
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
